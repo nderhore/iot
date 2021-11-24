d0b8840f4f0f692303820b6ad444ad575a97f990
Nathan <nathan@derhore.fr>
2021-11-24 16:41:13 +0100

    implements CreationTemperature when received Message

---
+++ src/main/java/fr/iot/derhore/rest/service/MQTTService.java
@@ -0,0 +1,118 @@
+package fr.iot.derhore.rest.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.iot.derhore.rest.entity.Temperature;
+import fr.iot.derhore.rest.manager.TemperatureManager;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MQTTService implements MqttCallback {
+
+    private MqttClient client = null;
+    private String mqttUserName = "iot-derhore", mqttPassword = "2y10rMY77U3FWqY19UbfMo43";
+    private String mqttIpAddress = "iot-derhore.cloud.shiftr.io";
+    private boolean mqttHaveCredential = true;
+    private String mqttPort = "1883";
+    private String mqttTopic = "appart/rdc/cuisine/four/temperature";
+    Logger LOG = LoggerFactory.getLogger(getClass());
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TemperatureManager temperatureManager;
+
+    @Override
+    public void connectionLost(Throwable arg0) {
+        LOG.info("connectionLost :" + arg0.getMessage()+" :"+arg0.toString());
+        LOG.info("retry ....");
+        this.startMqtt();
+    }
+
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken arg0) {
+        try {
+            LOG.info("deliveryCompleted " );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void messageArrived(String s, MqttMessage msg) throws Exception {
+        LOG.info("message Arrived !");
+        try {
+            Temperature temperature = objectMapper.readValue(msg.toString(), Temperature.class);
+            temperatureManager.createTemperature(temperature);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void publish(String topicSuffix, String content) {
+        MqttMessage message = new MqttMessage();
+        message.setPayload(content.getBytes());
+        message.setQos(2);
+        try {
+            String topic = mqttTopic + topicSuffix;
+            if (client.isConnected()) {
+                LOG.info("Connection Status :" + client.isConnected());
+            }
+            client.publish(topic, message);
+        } catch (MqttPersistenceException e) {
+            e.printStackTrace();
+
+        } catch (MqttException e) {
+            e.printStackTrace();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void startMqtt() {
+
+        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+            client = new MqttClient("tcp://" + mqttIpAddress + ":" + mqttPort, "SpringBootApp",
+                    persistence);
+        } catch (MqttException e1) {
+
+            System.out.println("error");
+            e1.printStackTrace();
+        }
+
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setCleanSession(true);
+        connectOptions.setMaxInflight(3000);
+        connectOptions.setAutomaticReconnect(true);
+        if (mqttHaveCredential) {
+            connectOptions.setUserName(mqttUserName);
+            connectOptions.setPassword(mqttPassword.toCharArray());
+        }
+
+        client.setCallback(this);
+
+        try {
+            IMqttToken mqttConnectionToken = client.connectWithResult(connectOptions);
+            client.subscribe("#");
+            LOG.info(" Connection Status :" + mqttConnectionToken.isComplete());
+
+        } catch (MqttException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+}
