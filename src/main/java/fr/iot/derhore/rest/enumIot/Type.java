bebfb841e25058ced4a52dc4b296c33e1e02a904
Nathan <nathan@derhore.fr>
2021-11-25 10:57:58 +0100

    fix serialize object mapper jackson

--- src/main/java/fr/iot/derhore/rest/enumIot/Type.java
+++ src/main/java/fr/iot/derhore/rest/enumIot/Type.java
@@ -1,7 +1,12 @@
 package fr.iot.derhore.rest.enumIot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 public enum Type {
 
+    @JsonProperty("AMPOULE")
     AMPOULE,
+    @JsonProperty("TEMPERATURE")
     TEMPERATURE
 }
